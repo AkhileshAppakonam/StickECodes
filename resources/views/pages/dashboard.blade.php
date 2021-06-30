520b96beafeac83f67fdb840239b7bda0089a5e7
EC2 Default User <ec2-user@ip-10-0-0-202.us-west-1.compute.internal>
2021-06-30 22:30:50 +0000

    Third Commit

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,8 +2,61 @@
 
 @section('content')
 <div class="container">
+    <header class="mb-2"><h1>Your Codes</h1></header>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4 col-sm-4">
+            <article class="dashboardCodes">
+                <figure class="mb-0">
+                    <div class="image"><a href="#"><img src="../images/stickecode.png"></a></div>
+                </figure>
+                <div class="description">
+                    <h3>Insert Title Here</h3>
+                    <p>Insert Title Description of Public Page Here</p>
+                    <hr>
+                    <div>
+                        <a href="#" class="btn btn-outline-dark">View Page</a>
+                        <a href="#" class="btn btn-outline-dark float-right">Edit Page</a>
+                    </div>
+                </div>
+            </article>
+        </div>
+
+        <div class="col-md-4 col-sm-4">
+            <article class="dashboardCodes">
+                <figure class="mb-0">
+                    <div class="image"><a href="#"><img src="../images/stickecode.png"></a></div>
+                </figure>
+                <div class="description">
+                    <h3>Insert Title Here</h3>
+                    <p>Insert Title Description of Public Page Here</p>
+                    <hr>
+                    <div>
+                        <a href="#" class="btn btn-outline-dark">View Page</a>
+                        <a href="#" class="btn btn-outline-dark float-right">Edit Page</a>
+                    </div>
+                </div>
+            </article>
+        </div>
+
+        <div class="col-md-4 col-sm-4">
+            <article class="dashboardCodes">
+                <figure class="mb-0">
+                    <div class="image"><a href="#"><img src="../images/stickecode.png"></a></div>
+                </figure>
+                <div class="description">
+                    <h3>Insert Title Here</h3>
+                    <p>Insert Title Description of Public Page Here</p>
+                    <hr>
+                    <div>
+                        <a href="#" class="btn btn-outline-dark">View Page</a>
+                        <a href="#" class="btn btn-outline-dark float-right">Edit Page</a>
+                    </div>
+                </div>
+            </article>
+        </div>
+        
+        
+        {{-- <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -17,7 +70,7 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
