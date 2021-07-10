7870301de7894595c73ece505d7d46232caaeb56
EC2 Default User <ec2-user@ip-10-0-0-202.us-west-1.compute.internal>
2021-07-10 04:08:23 +0000

    Sixteenth Committ

--- resources/views/pages/securityProfile.blade.php
+++ resources/views/pages/securityProfile.blade.php
@@ -3,13 +3,39 @@
 @section('content')
     <section id="securityProfiles">
         <div class="container">
-            <header class="mb-3"><h1>Your Security Profiles</h1></header>
+            <header class="mb-5"><h1>Your Security Profiles</h1></header>
 
-            <div class="row shadow p-3 mx-2 mb-5 bg-white rounded">
+            <div class="row shadow-lg ml-5 mr-3 mb-5 bg-lightblue rounded">
                 <div class="col-md-12 securityProfiles">
-                    <h3>this is a test</h3>
+                    <div class="container py-3 px-4" data-toggle="collapse" data-target="#securityProfile" aria-expanded="false" aria-controls="collapseExample" onclick="securityProfileHeader(this)">
+                        <h3>This is a test</h3>
+                        <a href="" class="btn btn-outline-dark">Edit Security Profile</a>
+                    </div>
+                    <div class="row mx-1 collapse" id="securityProfile">
+                        <div class="col-md-3 securityProfileCode">
+                            
+                        </div>
+                        <div class="col-md-3 securityProfileCode">
+                            <figure class="mb-0">
+                                <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
+                            </figure>
+                        </div>
+                        <div class="col-md-3 securityProfileCode">
+                            
+                        </div>
+                        <div class="col-md-3 securityProfileCode">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         </div>
     </section>
+
+    <script type="text/javascript">
+        function securityProfileHeader(e) {
+            e.classList.toggle("card-header");
+        }
+    </script>
 @endsection
