767a9cfd56a865d8a55589f6072fcd727424e46c
EC2 Default User <ec2-user@ip-10-0-0-202.us-west-1.compute.internal>
2021-07-11 02:06:02 +0000

    Eighteenth Commit

--- resources/views/pages/securityProfile.blade.php
+++ resources/views/pages/securityProfile.blade.php
@@ -5,75 +5,77 @@
         <div class="container">
             <header class="mb-5"><h1>Your Security Profiles</h1></header>
 
-            <div class="row shadow-lg ml-5 mr-3 mb-5 bg-lightblue rounded">
+            <div class="row shadow-lg ml-5 mr-3 mb-5 bg-white rounded">
                 <div class="col-md-12 securityProfiles">
                     <div class="container py-3 px-4" data-toggle="collapse" data-target="#securityProfile" aria-expanded="false" aria-controls="collapseExample" onclick="securityProfileHeader(this)">
-                        <h3>This is a test</h3>
-                        <a href="" class="btn btn-outline-dark">Edit Security Profile</a>
+                        <h3>Security Profile 1</h3>
+                        <a href="" class="btn btn-primary" onclick="stopProp(this)">Edit Security Profile</a>
                     </div>
                     <div class="row mx-1 collapse" id="securityProfile">
-                        <div class="col-md-3 securityProfileCode py-4">
-                            <article>
-                                <figure class="mb-0">
-                                    <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
-                                </figure>
-                                <div class="description px-5">
-                                    <div class="codeTitles">
-                                        <h3>Code Title</h3>
-                                        <small class="ml-1">XXXX</small>
+                        <div class="card-container">
+                            <div class="card">
+                                <article>
+                                    <figure class="mb-0">
+                                        <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
+                                    </figure>
+                                    <div class="description">
+                                        <div class="codeTitles">
+                                            <h3>Code Title</h3>
+                                            <small class="ml-1">XXXX</small>
+                                        </div>
+                                        <p class="ml-1">Insert Title Description for Public Page Here</p>
                                     </div>
-                                    <p class="ml-1">Insert Title Description for Public Page Here</p>
-                                </div>
-                            </article>
-                        </div>
-                        <div class="col-md-3 securityProfileCode py-4">
-                            <article>
-                                <figure class="mb-0">
-                                    <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
-                                </figure>
-                                <div class="description px-5">
-                                    <div class="codeTitles">
-                                        <h3>Code Title</h3>
-                                        <small class="ml-1">XXXX</small>
+                                </article>
+                            </div>
+                            <div class="card">
+                                <article>
+                                    <figure class="mb-0">
+                                        <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
+                                    </figure>
+                                    <div class="description">
+                                        <div class="codeTitles">
+                                            <h3>Code Title</h3>
+                                            <small class="ml-1">XXXX</small>
+                                        </div>
+                                        <p class="ml-1">Insert Title Description for Public Page Here</p>
                                     </div>
-                                    <p class="ml-1">Insert Title Description for Public Page Here</p>
-                                </div>
-                            </article>
-                        </div>
-                        <div class="col-md-3 securityProfileCode py-4">
-                            <article>
-                                <figure class="mb-0">
-                                    <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
-                                </figure>
-                                <div class="description px-5">
-                                    <div class="codeTitles">
-                                        <h3>Code Title</h3>
-                                        <small class="ml-1">XXXX</small>
+                                </article>
+                            </div>
+                            <div class="card">
+                                <article>
+                                    <figure class="mb-0">
+                                        <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
+                                    </figure>
+                                    <div class="description">
+                                        <div class="codeTitles">
+                                            <h3>Code Title</h3>
+                                            <small class="ml-1">XXXX</small>
+                                        </div>
+                                        <p class="ml-1">Insert Title Description for Public Page Here</p>
                                     </div>
-                                    <p class="ml-1">Insert Title Description for Public Page Here</p>
-                                </div>
-                            </article>
-                        </div>
-                        <div class="col-md-3 securityProfileCode py-4">
-                            <article>
-                                <figure class="mb-0">
-                                    <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
-                                </figure>
-                                <div class="description px-5">
-                                    <div class="codeTitles">
-                                        <h3>Code Title</h3>
-                                        <small class="ml-1">XXXX</small>
+                                </article>
+                            </div>
+                            <div class="card">
+                                <article>
+                                    <figure class="mb-0">
+                                        <div class="image"><img src="../images/stickecode.png" height="200px" width="200px"></div>
+                                    </figure>
+                                    <div class="description">
+                                        <div class="codeTitles">
+                                            <h3>Code Title</h3>
+                                            <small class="ml-1">XXXX</small>
+                                        </div>
+                                        <p class="ml-1">Insert Title Description for Public Page Here</p>
                                     </div>
-                                    <p class="ml-1">Insert Title Description for Public Page Here</p>
-                                </div>
-                            </article>
+                                </article>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            
+            </div> 
         </div>
     </section>
+
 
     <script type="text/javascript">
         var isclick = true;
@@ -85,6 +87,11 @@
                     isclick = true;
                 }, 350)
             }
+        }
+
+        function stopProp(e) {
+            $('#securityProfiles .container .securityProfiles .container').removeAttr('data-toggle');
+            $('#securityProfiles .container .securityProfiles .container').removeAttr('onclick');
         }
     </script>
 @endsection
