06181a5c564c31e1974e707457b7bdd24fdeff19
EC2 Default User <ec2-user@ip-10-0-0-202.us-west-1.compute.internal>
2021-07-14 06:47:06 +0000

    Twenty-first Commit

--- resources/views/pages/editSecurityProfile.blade.php
+++ resources/views/pages/editSecurityProfile.blade.php
@@ -17,7 +17,7 @@
                         <div class="container px-0 border-bottom">
                             <h2 class="py-3">Profile Users</h2>
                             <div class="pr-1">
-                                <input type="button" value="Add More Text" class="add btn btn-dark addRow" onclick="addText()">
+                                <input type="button" value="Add User" class="add btn btn-dark addRow" onclick="addUser()">
                             </div>
                         </div>
 
@@ -30,7 +30,7 @@
                                 <th>Remove Row</th>
                             </tr>
                             <tr class="mb-2 pb-3 border-bottom">
-                                <td><p>1.</p></td>
+                                <td><p></p></td>
                                 <td><input type="text" class="form-control" placeholder="Insert Email Address Here"></td>
                                 <td><input type="text" class="form-control" placeholder="Insert Name Here"></td>
                                 <td>
@@ -44,6 +44,11 @@
                             </tr>
                         </table>
                     </div>
+                    <div class="form-group col-12">
+                        <div class="submitContainer">
+                            <input type="submit" class="form-control btn btn-primary" value="Save Changes">
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
@@ -52,6 +57,16 @@
     <script type="text/javascript">
         function remove(e) {
             e.parentElement.parentElement.remove();
+        }
+
+        function addUser() {
+            var start = $('#editSecurityProfileTable'),
+                newRow = $('<tr class="mb-2 pb-3 border-bottom"><td><p></p></td>' +
+                        '<td><input type="text" class="form-control" placeholder="Insert Email Address Here"></td>' +
+                        '<td><input type="text" class="form-control" placeholder="Insert Name Here"></td>' +
+                        '<td><select id="choosePermissions" name="securityProfilePermissions" class="form-control"><option value="view">View Only</option><option value="update">View and Update</option><option value="full">Full Control</option></select></td>' +
+                        '<td><input type="button" value="Remove" class="remove btn btn-outline-dark" onclick="remove(this)"></td></tr>');
+            $(start).append(newRow);
         }
     </script>
 @endsection
