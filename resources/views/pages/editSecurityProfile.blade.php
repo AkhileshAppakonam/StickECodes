91b5277dde272cca6c880707b5ca4e95a9b5e89f
EC2 Default User <ec2-user@ip-10-0-0-202.us-west-1.compute.internal>
2021-07-14 00:57:33 +0000

    Twenty-First Commit

--- resources/views/pages/editSecurityProfile.blade.php
+++ resources/views/pages/editSecurityProfile.blade.php
@@ -14,7 +14,12 @@
                         <input id="securityProfileTitle" type="text" class="form-control" placeholder="Insert Profile Name Here" required>
                     </div>
                     <div class="form-group col-12">
-                        <h2 class="py-3 border-bottom">Profile Users</h2>
+                        <div class="container px-0 border-bottom">
+                            <h2 class="py-3">Profile Users</h2>
+                            <div class="pr-1">
+                                <input type="button" value="Add More Text" class="add btn btn-dark addRow" onclick="addText()">
+                            </div>
+                        </div>
 
                         <table id="editSecurityProfileTable">
                             <tr class="mb-3 pb-2 pt-1 border-bottom">
